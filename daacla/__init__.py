d5114cae1654c35418f3e5bf689aabed4c01dba5
anekos <anekos@snca.net>
2021-07-25 13:51:33 +0900

    Improve

--- daacla/__init__.py
+++ daacla/__init__.py
@@ -1,9 +1,13 @@
 
+from collections import defaultdict
 from dataclasses import dataclass
-from typing import Any, Dict, Optional, Tuple, Type, TypeVar
+from typing import Any, Callable, Dict, Optional, Tuple, Type, TypeVar, Union
 import re
 import sqlite3
 
+
+TableInstance = Any
+TableClass = Type[TableInstance]
 
 T = TypeVar('T')
 
@@ -14,30 +18,50 @@ class Meta:
     fields: Dict[str, Type]
     key: Optional[str] = None
 
-    @property
-    def ddl(self) -> str:
-        columns = []
+    def __hash__(self) -> int:
+        return id(self)
+
+    def __post_init__(self) -> None:
+        self.columns = {}
         for name, tipe in self.fields.items():
-            col = f'  {name} {_type_to_type(tipe)}'
+            col = f'{_type_to_type(tipe)}'
             if name == self.key:
                 col += ' PRIMARY KEY'
-            columns.append(col)
-        columns_part = ',\n'.join(columns)
-
-        return f"""CREATE TABLE IF NOT EXISTS {self.table} (
-{columns_part}
-)"""
+            self.columns[name] = col
 
     @property
-    def columns(self) -> str:
-        return ', '.join(self.fields.keys())
+    def ddl(self) -> str:
+        cs = map(lambda it: ' '.join(it), self.columns.items())
+        csj = ', '.join(cs)
+        return f"""CREATE TABLE IF NOT EXISTS {self.table} ({csj})"""
 
-    def values(self, instance) -> Tuple[Any, ...]:
+    def validate_key(self) -> str:
+        if self.key is None:
+            raise Exception(f'Primary key is not defined: {self.table}')
+        return self.key
+
+    def values(self, instance: TableInstance) -> Tuple[Any, ...]:
         return tuple(map(lambda it: getattr(instance, it), self.fields.keys()))
 
+    def set_values(self, instance: TableInstance, values: Dict[str, Any]) -> None:
+        for k, v in values.items():
+            setattr(instance, k, v)
 
-def table(key: Optional[str] = None):
-    def decorate(klass):
+
+def table(key: Optional[str] = None) -> Callable[[Type], TableClass]:
+    '''Decorator
+
+    import daacla
+
+    @dataclass
+    @daacla.table(key='a')
+    class HogeMoge:
+        a: str
+        b: int
+        c: Optional[bool] = None
+    '''
+
+    def decorate(klass: Type) -> TableClass:
         table = _snake_case(klass.__name__)
         klass.__daacla = Meta(
             key=key,
@@ -56,16 +80,19 @@ def _is_instance_of_daacla(instance: Any) -> bool:
     return hasattr(instance, '__daacla')
 
 
-def _get_meta(instance: Any) -> Meta:
-    if not _is_instance_of_daacla(instance):
-        raise Exception('Not a daacla instance. Use `@daacla` decorator')
-    return getattr(instance, '__daacla')
-
-
 def _type_to_type(t: Type) -> str:
+    # Datatypes In SQLite Version 3 - https://www.sqlite.org/datatype3.html
     if t == str:
-        return 'text'
-    return 'integer'
+        return 'TEXT'
+    if t == float:
+        return 'REAL'
+    if t == bool:
+        return 'BOOL'
+    return 'INTEGER'
+
+
+def _n_place_holders(n: int) -> str:
+    return ', '.join(['?'] * n)
 
 
 @dataclass
@@ -75,58 +102,67 @@ class Daacla:
     def __post_init__(self) -> None:
         if self.path is None:
             self.path = 'daacla.sqlite'
-        self._ready = False
-
-    def prepare(self, klass) -> Meta:
-        assert self.path is not None
-        meta = _get_meta(klass)
-
-        if not self._ready:
-            self.connection = sqlite3.connect(self.path, isolation_level=None)
-            self.connection.execute(meta.ddl)
-            self._ready = True
-
-        return meta
-
-    def drop_table(self, klass) -> None:
-        meta = self.prepare(klass)
-        self.connection.execute(f'DROP TABLE IF EXISTS {meta.table}')
-
-    def create_table(self, klass) -> None:
-        meta = self.prepare(klass)
-        self.connection.execute(meta.ddl)
-
-    def insert(self, instance) -> None:
-        meta = self.prepare(instance)
-        place_holders = ', '.join(['?'] * len(meta.fields.keys()))
-        self.connection.execute(
-            f'''INSERT INTO {meta.table} ({meta.columns}) VALUES ({place_holders})''',
-            meta.values(instance)
-        )
-
-    def update(self, instance, **kwargs) -> bool:
-        meta = self.prepare(instance)
-
-        if meta.key is None:
-            raise Exception(f'Primary key is not defined: {meta.table}')
-
-        for k, v in kwargs.items():
-            setattr(instance, k, v)
-
-        pairs = map(lambda kv: '='.join(kv), zip(meta.fields.keys(), ['?'] * len(meta.fields)))
-        key_value = getattr(instance, meta.key)
-        q = f'''UPDATE {meta.table} SET {', '.join(pairs)} WHERE {meta.key} = {key_value}'''
-        cur = self.connection.execute(q, meta.values(instance))
-
-        return cur.rowcount == 1
+        self.connection = sqlite3.connect(self.path, isolation_level=None)
+        self._ready: Dict[Meta, bool] = defaultdict(bool)
 
     def get(self, klass: Type[T], key: Any) -> Optional[T]:
-        meta = _get_meta(klass)
-        q = f"""SELECT {meta.columns} FROM {meta.table} WHERE {meta.key} = {key}"""
+        meta = self.prepare_table(klass)
+        key_column = meta.validate_key()
+        q = f"""SELECT {', '.join(meta.fields.keys())} FROM {meta.table} WHERE {key_column} = ?"""
         cur = self.connection.cursor()
-        for t in cur.execute(q):
+        for t in cur.execute(q, (key,)):
             params = {}
             for k, v in zip(meta.fields.keys(), t):
                 params[k] = v
             return klass(**params)  # type: ignore
         return None
+
+    def truncate(self, klass: TableClass) -> None:
+        meta = self.prepare_table(klass)
+        self.connection.execute(f'DELETE FROM {meta.table}')
+
+    @staticmethod
+    def in_memory(**kwargs: Any) -> 'Daacla':
+        return Daacla(path=':memory:', **kwargs)  # type: ignore
+
+    def insert(self, instance: TableInstance) -> None:
+        meta = self.prepare_table(instance)
+        place_holders = ', '.join(['?'] * len(meta.fields.keys()))
+        self.connection.execute(
+            f'''INSERT INTO {meta.table} ({', '.join(meta.fields.keys())}) VALUES ({place_holders})''',
+            meta.values(instance)
+        )
+
+    def meta(self, instance_or_klass: Union[TableInstance, TableClass]) -> Meta:
+        if not _is_instance_of_daacla(instance_or_klass):
+            raise Exception('Not a Daacla instance. Use `@daacla.table` decorator')
+        return getattr(instance_or_klass, '__daacla')
+
+    def prepare_table(self, klass: TableClass) -> Meta:
+        meta = self.meta(klass)
+
+        if not self._ready[meta]:
+            self.connection.execute(meta.ddl)
+            self._ready[meta] = True
+
+        return meta
+
+    def update(self, instance: TableInstance, **kwargs: Any) -> bool:
+        meta = self.prepare_table(instance)
+        key_column = meta.validate_key()
+        meta.set_values(instance, kwargs)
+
+        key_value = getattr(instance, key_column)
+        pairs = map(lambda kv: '='.join(kv), zip(meta.fields.keys(), ['?'] * len(meta.fields)))
+        q = f'''UPDATE {meta.table} SET {', '.join(pairs)} WHERE {key_column} = ?'''
+        cur = self.connection.execute(q, (*meta.values(instance), key_value))
+        return cur.rowcount == 1
+
+    def upsert(self, instance: TableInstance, **kwargs: Any) -> bool:
+        meta = self.prepare_table(instance)
+        _ = meta.validate_key()
+        meta.set_values(instance, kwargs)
+
+        q = f'''REPLACE INTO {meta.table} ({', '.join(meta.fields.keys())}) VALUES ({_n_place_holders(len(meta.fields))})'''
+        cur = self.connection.execute(q, meta.values(instance))
+        return cur.rowcount == 1
